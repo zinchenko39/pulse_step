3d98e1501c46a9709713a064ca8c574ac0200693
Vadim Zinchenko <vadim.zinchenco@mail.ru>
2022-06-02 12:59:53 +0000

    finish slider & add section "catalog"

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
+    controls: false,
+    nav: false,
 });
+
+document.querySelector('.prev').addEventListener("click", () =>  slider.goTo("prev"));
+document.querySelector('.next').addEventListener("click", () => slider.goTo("next"));
